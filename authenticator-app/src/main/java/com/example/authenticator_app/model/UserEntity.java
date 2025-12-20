25d645f7605083bb451101e9712ba7c8a040ae13
ZeroGravityDesign <loieschristo@gmail.com>
2025-12-20 20:26:29 +0530

    post reqeust user name and password

--- authenticator-app/src/main/java/com/example/authenticator_app/model/UserEntity.java
+++ authenticator-app/src/main/java/com/example/authenticator_app/model/UserEntity.java
@@ -1,21 +1,20 @@
 package com.example.authenticator_app.model;
 
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.sql.Timestamp;
 
 @Entity
 @Table(name = "tbl_users")
